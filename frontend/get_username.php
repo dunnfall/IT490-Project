9260f74ef4212a35d7566433860b73806aef0bab
Daniel Fernandez <143975460+dunnfall@users.noreply.github.com>
2025-02-28 18:29:06 -0500

    Delete frontend/get_username.php

--- frontend/get_username.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-$response = ['username' => isset($_SESSION['username']) ? $_SESSION['username'] : null];
-echo json_encode($response);
-?>
